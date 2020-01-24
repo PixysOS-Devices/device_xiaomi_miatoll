35f71a63fe9f7481ccd9755731b6101660640951
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    miatoll: Use Pixel charger animation
    Signed-off-by: Arseniy Graur <argraur@gmail.com>
    Change-Id: Ie0d25e37912fffd337731d4c5eb9e9ccf09a2696

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, device/xiaomi/sm6250-common/common.mk)
 # Inherit proprietary targets
 $(call inherit-product-if-exists, vendor/xiaomi/miatoll/miatoll-vendor.mk)
 
+# Charger
+PRODUCT_PACKAGES += \
+    product_charger_res_images
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
